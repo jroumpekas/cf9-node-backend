f1fead6c89becc5946c9655e16c0f70d53223bfc
Dim.Roumpekas <jimroumpi@gmail.com>
2026-04-22 18:46:57 +0300

    feat: add jwt

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import * as authService from '../services/auth.service';
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, password } = req.body;
+
+  const result = await authService.login(username, password);
+
+    if (!result) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    return res.status(200).json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
